ab4b3e50cd63da55df1fcfd01741ca64b19be9ec
Spencer <spencerartisan@gmail.com>
2013-09-03 22:45:03 +0100

    Time found now handles yesterday and earlier

--- app/orphan.rb
+++ app/orphan.rb
@@ -6,7 +6,18 @@ Orphan = Struct.new(:lat, :lon, :description, :image_url, :found_at) do
   end
 
   def subtitle
-    "Found today at #{found_at.strftime('%H:%M')}"
+    now = Time.now
+    day_found = Time.new found_at.year, found_at.month, found_at.mday
+    today = Time.new now.year, now.month, now.mday
+    interval = today.timeIntervalSinceDate day_found
+    days_ago = (interval / (60 * 60 * 24)).to_i
+    day = case days_ago 
+          when 0; 'today'
+          when 1; 'yesterday'
+          else; "#{days_ago} days ago"
+          end
+    time = found_at.strftime('%H:%M')
+    "Found #{day} at #{time}"
   end
 end
 
--- spec/orphan_spec.rb
+++ spec/orphan_spec.rb
@@ -1,8 +1,22 @@
 describe Orphan do
+  before do
+    @orphan = Orphan.new 
+    @now = Time.now
+    @today_1540 = Time.gm @now.year, @now.month, @now.mday, 15, 40
+  end
+
   it 'should create a subtitle for items found today' do
-    orphan = Orphan.new 
-    now = Time.now
-    orphan.found_at = Time.gm now.year, now.month, now.mday, 15, 40
-    orphan.subtitle.should == 'Found today at 15:40'
+    @orphan.found_at = @today_1540
+    @orphan.subtitle.should == 'Found today at 15:40'
+  end
+
+  it 'should create a subtitle for items found yesterday' do
+    @orphan.found_at = @today_1540 - ( 60 * 60 * 24)
+    @orphan.subtitle.should == 'Found yesterday at 15:40'
+  end
+
+  it 'should create a subtitle for items found before yesterday' do
+    @orphan.found_at = @today_1540 - ( 60 * 60 * 24 * 2)
+    @orphan.subtitle.should == 'Found 2 days ago at 15:40'
   end
 end
