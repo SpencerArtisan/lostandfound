36b1c0bba1263a3b2f89713c2a9717a0331fbedd
Spencer <spencerartisan@gmail.com>
2013-09-11 21:24:45 +0100

    point to heroku backend

--- app/orphanage_server.rb
+++ app/orphanage_server.rb
@@ -1,6 +1,8 @@
 class OrphanageServer
+  URL = 'http://lostandfoundserver.herokuapp.com/orphans.json'
+
   def all &block
-    BW::HTTP.get('http://lostandfound.aws.af.cm/orphans.json') do |response|
+    BW::HTTP.get(URL) do |response|
       puts "RESPONSE"
       puts response
       block.call response.body.to_str
@@ -10,7 +12,7 @@ class OrphanageServer
   def save orphan
     payload = {payload: {orphan: orphan}}
     puts "Posting new orphan #{payload}"
-    BW::HTTP.post('http://lostandfound.aws.af.cm/orphans.json', payload) do |response|
+    BW::HTTP.post(URL, payload) do |response|
       p response
     end
   end
